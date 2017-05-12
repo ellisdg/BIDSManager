08e9c10494a99a4b02adfcc201219224dcd21e31
David Ellis <david.ellis@unmc.edu>
2017-05-12 09:11:45 -0500

    FIX: moves travis testing script to a bash file

---
+++ .travis.sh
@@ -0,0 +1,8 @@
+cd TEST
+nosetests test_dataSet.py
+nosetests test_group.py
+nosetests test_image.py
+nosetests test_reader.py
+nosetests test_session.py
+nosetests test_subject.py
+nosetests test_write.py
