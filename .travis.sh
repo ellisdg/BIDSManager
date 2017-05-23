d3be1e9e7bc53c6a90b016e74f7270726f8d1c41
David G Ellis <ellisdg@users.noreply.github.com>
2017-05-23 14:28:28 -0500

    Adds codecov

--- .travis.sh
+++ .travis.sh
@@ -8,3 +8,4 @@ nosetests NoseTests/test_reader.py
 nosetests NoseTests/test_session.py
 nosetests NoseTests/test_subject.py
 nosetests NoseTests/test_write.py
+bash <(curl -s https://codecov.io/bash)
