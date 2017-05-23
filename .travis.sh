41835101cf0c306df9eaeb7976ce8ea6adfdc1ae
David G Ellis <ellisdg@users.noreply.github.com>
2017-05-23 14:38:42 -0500

    Adds coverage to nosetests

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,11 @@
 PackageDir=${PWD}
 export PYTHONPATH=${PackageDir}:${PYTHONPATH}
 cd TEST
-nosetests NoseTests/test_dataSet.py
-nosetests NoseTests/test_group.py
-nosetests NoseTests/test_image.py
-nosetests NoseTests/test_reader.py
-nosetests NoseTests/test_session.py
-nosetests NoseTests/test_subject.py
-nosetests NoseTests/test_write.py
+nosetests --with-coverage NoseTests/test_dataSet.py
+nosetests --with-coverage NoseTests/test_group.py
+nosetests --with-coverage NoseTests/test_image.py
+nosetests --with-coverage NoseTests/test_reader.py
+nosetests --with-coverage NoseTests/test_session.py
+nosetests --with-coverage NoseTests/test_subject.py
+nosetests --with-coverage NoseTests/test_write.py
 codecov --token=69751b3a-1010-436b-86a6-298d9bbf6364
