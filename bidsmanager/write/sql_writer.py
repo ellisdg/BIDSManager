3d067c7e6b19b3c360b0dfb39a46bee2ea354ade
David Ellis <david.ellis@unmc.edu>
2017-05-25 17:12:15 -0500

    Deletes unused file

--- bidsmanager/write/sql_writer.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-def connect_to_database(sql_file):
-    return sqlite3.connect(sql_file)
-
-
-def execute_statement(connection, sql_statement):
-    return connection.cursor().execute(sql_statement)
