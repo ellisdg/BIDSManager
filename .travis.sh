2e7889b04e7dfba617937d52faba8b85b46e3bc6
David Ellis <david.ellis@unmc.edu>
2017-05-26 09:26:17 -0500

    ENH: Removes travis script

--- .travis.sh
+++
@@ -1,5 +0,0 @@
-PackageDir=${PWD}
-export PYTHONPATH=${PackageDir}:${PYTHONPATH}
-cd TEST
-nosetests --with-coverage --exclude=NoseTests/test_dicomreader.py
-codecov --token=69751b3a-1010-436b-86a6-298d9bbf6364
