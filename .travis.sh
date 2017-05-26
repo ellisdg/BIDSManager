315a2e9fdb59e7f06b3365050f5cf62e86359c47
David Ellis <david.ellis@unmc.edu>
2017-05-26 09:12:47 -0500

    FIX: Ignores dicom reader tests

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,5 @@
 PackageDir=${PWD}
 export PYTHONPATH=${PackageDir}:${PYTHONPATH}
 cd TEST
-nosetests --with-coverage NoseTests/test_dataSet.py
-nosetests --with-coverage NoseTests/test_group.py
-nosetests --with-coverage NoseTests/test_image.py
-nosetests --with-coverage NoseTests/test_reader.py
-nosetests --with-coverage NoseTests/test_session.py
-nosetests --with-coverage NoseTests/test_subject.py
-nosetests --with-coverage NoseTests/test_write.py
+nosetests --with-coverage --exclude=NoseTests/test_dicomreader.py
 codecov --token=69751b3a-1010-436b-86a6-298d9bbf6364
