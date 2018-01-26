aaf8ed666469e3e9461bf5224adefbccaa4da76f
David Ellis <david.ellis@unmc.edu>
2018-01-26 09:52:54 -0600

    Adds another test

--- TEST/NoseTests/test_image.py
+++ TEST/NoseTests/test_image.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 from bidsmanager.base.image import Image, FunctionalImage
 from bidsmanager.read.image_reader import read_image_from_bids_path
+from bidsmanager.write.dataset_writer import write_json
 
 
 def touch(tmp_file):
@@ -42,3 +43,32 @@ class TestImage(TestCase):
             if os.path.exists(new_tmp_file):
                 os.remove(new_tmp_file)
             raise error
+
+    def test_write_metadata(self):
+        tmp_file = "run-05_FLAIR.nii.gz"
+        touch(tmp_file)
+        tmp_json = "run-05_FLAIR.json"
+        files_to_delete = [tmp_file, tmp_json]
+        json_data = {"the_answer": 42}
+        write_json(json_data, tmp_json)
+        image = read_image_from_bids_path(tmp_file)
+        self.assertEqual(image.get_metadata(), json_data)
+
+        image._run_number = 6
+        image.update(move=False)
+        files_to_delete.append(image.get_path())
+        files_to_delete.append(image.sidecar_path)
+        self.assertEqual(image.get_metadata(), json_data)
+        self.assertTrue(os.path.exists(image.sidecar_path))
+
+        prev_sidecar_path = image.sidecar_path
+        image._run_number = 7
+        image.update(move=True)
+        files_to_delete.append(image.get_path())
+        files_to_delete.append(image.sidecar_path)
+        self.assertEqual(image.get_metadata(), json_data)
+        self.assertFalse(os.path.exists(prev_sidecar_path))
+
+        for f in set(files_to_delete):
+            if os.path.exists(f):
+                os.remove(f)
