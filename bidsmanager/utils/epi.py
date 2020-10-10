ea7cddaab758d6da7421eae9311600c05503a16f
david.ellis <david.ellis@unmc.edu>
2020-10-10 18:03:46 -0500

    ENH: adds function for adding intended for to metadata

---
+++ bidsmanager/utils/epi.py
@@ -0,0 +1,6 @@
+
+
+def set_intended_for(image, intended_for):
+    intended_for_path = intended_for.get_path().replace(intended_for.get_subject().get_path(), "").strip("/")
+    image.add_metadata("IntendedFor", intended_for_path)
+
