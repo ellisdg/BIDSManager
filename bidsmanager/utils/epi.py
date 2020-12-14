79ad0c5dba79983bb56e8a6fe51b81676149bba6
David Ellis <david.ellis@unmc.edu>
2020-12-14 11:15:07 -0600

    FIX

--- bidsmanager/utils/epi.py
+++ bidsmanager/utils/epi.py
@@ -3,6 +3,6 @@
 def set_intended_for(image, intended_for):
     intended_for_path = intended_for.get_path().replace(
         intended_for.get_subject().get_path(), "").strip("/")
-    if image.get_metadata("IntendedFor") != intended_for_path:
+    if "IntendedFor" in image.get_metadata() and image.get_metadata("IntendedFor") != intended_for_path:
         image.add_metadata("IntendedFor", intended_for_path)
 
