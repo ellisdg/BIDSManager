0248b46ec07140d28b768220b9d5ac588d281ea3
David Ellis <david.ellis@unmc.edu>
2020-12-14 10:52:45 -0600

    ENH: does not set intended if it is already correct

--- bidsmanager/utils/epi.py
+++ bidsmanager/utils/epi.py
@@ -3,5 +3,6 @@
 def set_intended_for(image, intended_for):
     intended_for_path = intended_for.get_path().replace(
         intended_for.get_subject().get_path(), "").strip("/")
-    image.add_metadata("IntendedFor", intended_for_path)
+    if image.get_metadata("IntendedFor") != intended_for_path:
+        image.add_metadata("IntendedFor", intended_for_path)
 
