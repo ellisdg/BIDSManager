d75949234f65a5ae8f16bab89314e0ccb3eaa02e
David G Ellis <david.ellis@unmc.edu>
2026-03-10 11:14:54 -0500

    update

--- bidsmanager/read/dicom_reader.py
+++ bidsmanager/read/dicom_reader.py
@@ -3,6 +3,7 @@ import glob
 from subprocess import Popen, PIPE
 import random
 from warnings import warn
+import datetime
 
 from ..base.subject import Subject
 from ..base.dataset import DataSet
@@ -10,17 +11,22 @@ from ..base.session import Session
 from ..utils.image_utils import load_image
 
 
-def parse_image_keys(in_file, description, heuristic):
+def parse_image_keys(in_file, description, heuristic, case_sensitive=False):
     """
     Parse the image description (and in the future other image information) to
     determine the bids keys based on a heuristic provided by the user.
     :param in_file: nifti file output from dcm2niix
     :param description: Series Description provided in the filename from dcm2niix
     :param heuristic: user provided heuristic to get the keys from the image information
+    :param case_sensitive: If True, matching of SeriesDescription will be case sensitive.
     :return: image_keys in dictionary format (will be None if image should be skipped)
     """
     image_keys = dict()
+    if not case_sensitive:
+        description = description.lower()
     for test_heuristic, test_keys in heuristic["SeriesDescription"]:
+        if not case_sensitive:
+            test_heuristic = test_heuristic.lower()
         if test_heuristic in description:
             if test_keys is None:
                 print("{} found in {}. Skipping: {}".format(test_heuristic,
@@ -50,9 +56,17 @@ def parse_output(in_file, separator):
     return os.path.basename(in_file).split(separator)
 
 
-def get_image(in_file, separator, heuristic):
+def get_image(in_file, separator, heuristic, case_sensitive=False):
+    """
+    Get the image from the dcm2niix output file in BIDSManager format.
+    :param in_file:
+    :param separator:
+    :param heuristic:
+    :param case_sensitive: If True, matching of SeriesDescription will be case sensitive.
+    :return:
+    """
     subject_name, time, description, protocol, run = parse_output(in_file, separator)
-    image_keys = parse_image_keys(in_file, description, heuristic=heuristic)
+    image_keys = parse_image_keys(in_file, description, heuristic=heuristic, case_sensitive=case_sensitive)
     # returns None if no image modality is found or image is to be skipped
     if image_keys:
         bval_path = get_secondary_output(in_file, ".nii.gz", ".bval")
@@ -68,7 +82,23 @@ def convert_dicom_directory(input_directory,
                             separator="---",
                             bids_directory=None,
                             delete_intermediates=True,
-                            verbose=False):
+                            verbose=False,
+                            use_session_dates=False,
+                            case_sensitive=False):
+    """
+    Convert a directory of DICOM files to BIDS format using dcm2niix.
+    :param input_directory:
+    :param heuristic:
+    :param anonymize:
+    :param separator:
+    :param bids_directory:
+    :param delete_intermediates:
+    :param verbose:
+    :param use_session_dates: If True, use the acquisition date to create session names.
+    Default behavior is to put all the images in a single session. (default: False)
+    :param case_sensitive: If True, matching of SeriesDescription will be case sensitive. (default: False)
+    :return:
+    """
     # TODO: add option to specify a subject name to subject ID mapping
     output_directory = random_tmp_directory()
     run_dcm2niix_on_directory(input_directory, output_directory, filename="%n{0}%t{0}%d{0}%p{0}".format(separator),
@@ -84,7 +114,17 @@ def convert_dicom_directory(input_directory,
             subject = Subject(subject_name)
             dataset.add_subject(subject)
 
-        session_name = ""
+        # parse acquisition date if use_session_dates is True
+        if use_session_dates:
+            # the variable time is expected to be in the format YYYYMMDDHHMMSS
+            try:
+                acquisition_date = datetime.datetime.strptime(time, "%Y%m%d%H%M%S")
+                session_name = acquisition_date.strftime("%Y%m%d")
+            except ValueError:
+                warn(RuntimeWarning("Invalid time format: {}. Using empty session name.".format(time)))
+                session_name = ""
+        else:
+            session_name = ""
         # TODO: keep track of date/times and sort sessions
         #       by dates, then anonymize them into session numbers.
 
@@ -96,8 +136,8 @@ def convert_dicom_directory(input_directory,
             subject.add_session(session)
 
         # add image
-        image = get_image(f, separator, heuristic=heuristic)
-        # TODO: test for duplicates
+        image = get_image(f, separator, heuristic=heuristic, case_sensitive=case_sensitive)
+        # TODO: Add SBRef images to a certain group
         if image:
             session.add_image(image)
 
